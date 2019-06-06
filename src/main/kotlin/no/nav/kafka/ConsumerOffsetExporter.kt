9d0b848e9ee4fabfc2d06a84c89fc78c9886d74d
Geir André Lund <geir.andre.lund@nav.no>
2019-06-06 06:44:34 +0200

    log group - debug

--- src/main/kotlin/no/nav/kafka/ConsumerOffsetExporter.kt
+++ src/main/kotlin/no/nav/kafka/ConsumerOffsetExporter.kt
@@ -75,7 +75,7 @@ class ConsumerOffsetExporter(environment: Environment) {
                     val lag = getLogEndOffset(topicPartition) - currentOffset
                     offsetLagGauge.labels(group, topicPartition.partition().toString(), topicPartition.topic())
                         .set(lag.toDouble())
-                    LOGGER.info("Lag is -> $lag for topic '${topicPartition.topic()}', partition ${topicPartition.partition()}, current offset $currentOffset")
+                    LOGGER.info("Lag is -> $lag for topic '${topicPartition.topic()}', partition ${topicPartition.partition()}, current offset $currentOffset for group: $group" )
                 }
                 throwable?.apply {
                     LOGGER.error(throwable) { "Failed to get offset data from consumer group $group" }
